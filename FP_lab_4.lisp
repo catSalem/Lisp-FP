25c5c7c2107a9d0a82f09e2ea3dfc6a61d05974e
catSalem <Galdarkk@gmail.com>
2017-03-11 19:45:31 +0200

    lab4_upd
    - check for "is it LIST?"
    - check for list-length to bypass transformation with odd number of list
    elements:
    (a b c d e) --> ((a b) (c d) (e nil))

--- FP_lab_4.lisp
+++ FP_lab_4.lisp
@@ -1,5 +1,6 @@
 (defun lab_4 (list_to_transform)
 	(cond
 		((null list_to_transform) nil)
-		((atom list_to_transform) 'List_Not_Atom)
+		((not (listp list_to_transform)) 'List_Not_Atom)
+		((= (rem (list-length list_to_transform) 2) 1) 'Amount_of_list_elements_must_be_EVEN!)
 		(t (append (list (list (car list_to_transform) (car (cdr list_to_transform)))) (lab_4 (cdr (cdr list_to_transform)))))))
